ddecc335ae29dc29bf2987683e455205a3b6d15e
andrewtatomyr <andrew.tatomyr@gmail.com>
2016-06-23 19:57:59 +0300

    Initial structure

---
+++ imports/ui/pages/home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Header from '../components/Header.jsx';
+
+// App component - represents the whole app
+export default class HomePage extends React.Component {
+
+  render() {
+    return (
+      <div className="container">
+        <Header />
+
+
+      </div>
+    );
+  }
+
+}
